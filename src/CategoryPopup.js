c53da94ae5f08dcd1054a06a8aaa6bd3c6e4bfd8
Archi00 <albert.archi@gmail.com>
2022-07-07 10:31:46 +0200

    Changed add category bar style

--- src/CategoryPopup.js
+++ src/CategoryPopup.js
@@ -55,13 +55,12 @@ export default class CategoryPopup extends React.Component {
   render() {
     return (
       <div className="popup">
-        <form onSubmit={this.handleSubmit} ref={this.wrapperRef}>
-          <input
-            className="book-input"
-            name="title"
-            onChange={(this.props.handleChange, this.handleChange)}
-            autoFocus
-          />
+        <form onSubmit={this.handleSubmit} ref={this.wrapperRef} className="my-[40vh] block m-auto w-[50%]">   
+            <label for="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Add Category</label>
+            <div className="relative">
+              <input onChange={(this.props.handleChange, this.handleChange)} name="title" autoFocus type="search" id="search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Add Category" required=""></input>
+              <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+            </div>
         </form>
         {this.state.catAdded ? (
           <div className="feedback-cat">
