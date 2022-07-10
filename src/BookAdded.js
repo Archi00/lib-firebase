7735b12ed22d39ca3e036a059e43ddf2638a484e
Archi00 <albert.archi@gmail.com>
2022-07-10 23:47:18 +0200

    Added markup for notifications

--- src/BookAdded.js
+++
@@ -1,9 +0,0 @@
-const BookAdded = (numOfBooks) => {
-  return (
-    <div className="relative top-0 bottom-0 m-auto bg-gray-800">
-      <h1 className="uppercase text-bold text-4xl text-bold">{numOfBooks} added successfully!</h1>
-    </div>
-  )
-}
-
-export default BookAdded
