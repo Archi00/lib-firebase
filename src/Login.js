a4f1e46e2a9478dcc3ba5619fa0d15b3c7eb4648
Archi00 <albert.archi@gmail.com>
2022-07-11 19:53:45 +0200

    Added functionallity to the login page

--- src/Login.js
+++ src/Login.js
@@ -27,17 +27,17 @@ function Login(props) {
         <form class="flex flex-col space-y-12 pt-2" action="#">
             <div>
                 <label for="email" class="block mb-2 text-3xl font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@placeholder.com" required />
             </div>
             <div className="pb-24">
                 <label for="password" class="block mb-2 text-3xl font-medium text-gray-900 dark:text-gray-300">Your password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                <input onChange={() => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
             </div>
-            <button type="submit" class="w-full text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-800">Login to your account</button>
+            <button onClick={() => signInWithEmailPassword(email, password)} type="submit" class="w-full text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-800">Login to your account</button>
             <div class="text-xl font-medium text-gray-500 dark:text-gray-300">
-                Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                Not registered? <a href="/reset" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
               <div>
-                <a href="#" class="ml-auto my-8 text-lg text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                <a href="/register" class="ml-auto my-8 text-lg text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
               </div>
             </div>
         </form>
