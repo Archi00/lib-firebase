7f5fe408783a22f824ec16b67548ec6595d213aa
Archi00 <albert.archi@gmail.com>
2022-07-11 19:18:08 +0200

    Changed style for login page

--- src/Login.js
+++ src/Login.js
@@ -23,37 +23,55 @@ function Login(props) {
 
   return (
     <div className="login">
-      <div className="login__container">
-        <input
-          type="text"
-          className="login__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="login__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button
-          className="login__btn"
-          onClick={() => {
-            signInWithEmailPassword(email, password);
-          }}
-        >
-          Login
-        </button>
-        <div>
-          <Link to="/reset">Forgot Password</Link>
-        </div>
-        <div>
-          Don't have an account? <Link to="/register">Register</Link> now.
-        </div>
+      <div class="text-center p-14 max-w-[40vh] min-w-[40vh] min-h-[45vh] max-h-[45vh] bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-600">
+        <form class="flex flex-col space-y-12 pt-2" action="#">
+            <div>
+                <label for="email" class="block mb-2 text-3xl font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+            </div>
+            <div className="pb-24">
+                <label for="password" class="block mb-2 text-3xl font-medium text-gray-900 dark:text-gray-300">Your password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+            </div>
+            <button type="submit" class="w-full text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-2xl px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-800">Login to your account</button>
+            <div class="text-xl font-medium text-gray-500 dark:text-gray-300">
+                Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+              <div>
+                <a href="#" class="ml-auto my-8 text-lg text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+              </div>
+            </div>
+        </form>
       </div>
     </div>
   );
 }
 export default Login;
+
+        // <input
+        //   type="text"
+        //   className="login__textBox"
+        //   value={email}
+        //   onChange={(e) => setEmail(e.target.value)}
+        //   placeholder="E-mail Address"
+        // />
+        // <input
+        //   type="password"
+        //   className="login__textBox"
+        //   value={password}
+        //   onChange={(e) => setPassword(e.target.value)}
+        //   placeholder="Password"
+        // />
+        // <button
+        //   className="login__btn"
+        //   onClick={() => {
+        //     signInWithEmailPassword(email, password);
+        //   }}
+        // >
+        //   Login
+        // </button>
+        // <div>
+        //   <Link to="/reset">Forgot Password</Link>
+        // </div>
+        // <div>
+        //   Don't have an account? <Link to="/register">Register</Link> now.
+        // </div>
