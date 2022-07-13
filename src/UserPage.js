c5050136177aff31c05a88984b413a145f9b0f80
Archi00 <albert.archi@gmail.com>
2022-07-13 15:49:47 +0200

    Added base logic for public user page

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,17 +1,38 @@
 import { useEffect, useState } from "react"
 import getUserData from "./getUserData"
+import { filterCategories } from "./utils"
+import SideBar from "./SideBar"
 
 const UserPage = ({user}) => {
   const [userData, setUserData] = useState([])
+  const [allBooks, setAllBooks] = useState(true)
 
   useEffect(async () => {
     const data = await getUserData(user)
     setUserData(data)
-    console.log(data)
-  }, []) 
+  }, [])
+
+  const handleSelected = (bool) => {
+    setAllBooks(bool)
+  } 
+
+  const handleHomeBtn = () => {
+   console.log("back button") 
+  }
+
+  const handleBackBtn = () => {
+    console.log("clear filters")
+  }
+
 
   return (
-    <h1>Hello World!</h1>
+    <div className="flex flex-row content-start ">
+      <div className="flex flex-1 max-w-[15vw] min-w-[15vw]">
+        <SideBar filterCategories={filterCategories} handleHomeBtn={handleHomeBtn} handleBackBtn={handleBackBtn} handleSelected={handleSelected} />
+      </div>
+      <div className="flex flex-9 mt-[8vh] mx-auto">
+      </div>
+    </div>
   )
 }
 
--- src/utils.js
+++ src/utils.js
@@ -33,4 +33,72 @@ function debounce(func, ms = 300) {
   };
 }
 
-export { capitalize, FetchUserName, classNames, debounce };
+  const getData = async (filter, value) => {
+    let temp = [];
+    if (filter === "category") {
+      for (let i = 0, l = props.info.length; i < l; i++) {
+        if (props.info[i].data.name.toUpperCase().includes(value.toUpperCase())) {
+          temp.push(props.info[i]);
+        }
+      }
+      return temp;
+    } 
+    for (let i = 0, l = props.info.length; i < l; i++) {
+      if (props.info[i].data.books) {
+        props.info[i].data.books.map((e) => {
+            if (typeof e[filter] === "string") {
+              if (e[filter].toUpperCase().includes(value.toUpperCase())) {
+                temp.push(e);
+              }
+            } else if (typeof e[filter] === "object") {
+              Object.values(e[filter]).forEach((y) => {
+                if (!y.identifier) {
+                  if (
+                    e[filter][0].toUpperCase().includes(value.toUpperCase())
+                  ) {
+                    temp.push(e);
+                  }
+                } else {
+                  if (y.identifier.includes(value)) {
+                    temp.push(e);
+                  }
+                }
+              });
+            }
+        });
+      }
+    }
+    return temp;
+    
+  };
+
+  const filterCategories = (e) => {
+    let temp = []
+    if (e.target.value === "" || !e.target.value) {
+      cleanFilters();
+      return
+    }
+    const name = e.target.name.split("-").pop();
+    e.target.parentNode.childNodes.forEach((a) => {
+      if (a.nodeName === "INPUT" && name === "category") {
+        if (!a.value) {
+          cleanFilters();
+          return
+        }
+        getData(a.name.split("-").pop(), a.value.toUpperCase()).then((res, err) => {
+          res.map(r => temp.push([...catFilters, r.data?.name]))
+        });
+      } else if (a.nodeName === "INPUT" ) {
+        if (!a.value.toUpperCase()) {
+          cleanFilters();
+          return
+        }
+        getData(a.name.split("-").pop(), a.value.toUpperCase()).then((res, err) => {
+          if (a.value.toUpperCase) return res;
+        });
+      }
+    });
+    return temp
+  };
+
+export { capitalize, FetchUserName, classNames, debounce, filterCategories, getData };
